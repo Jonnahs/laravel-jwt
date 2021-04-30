21b56a75ab8bbfca5699b5351171533530db4ae6
Jonnahs <jonas.desenvolvimento@gmail.com>
2021-04-30 12:18:17 -0300

    Implementada todas as rotas

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::group(
+    [
+        'middleware' => 'api',
+        'namespace'  => 'App\Http\Controllers',
+        'prefix'     => 'auth',
+    ],
+    function ($router) {
+        Route::post('login', 'AuthController@login');
+        Route::post('register', 'AuthController@register');
+        Route::post('logout', 'AuthController@logout');
+        Route::get('profile', 'AuthController@profile');
+        Route::post('refresh', 'AuthController@refresh');
+    }
+);
+
+Route::group(
+    [
+        'middleware' => 'api',
+        'namespace'  => 'App\Http\Controllers',
+    ],
+    function ($router) {
+        Route::resource('todos', 'TodoController');
+    }
+);
